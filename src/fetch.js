79452440eff5b9077d99be2b06e6ddd0b3ac633c
oprogramador <uomodislesia@gmail.com>
2024-05-20 03:00:29 +0200

    removeMeaningless

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,7 @@ const request = require('superagent');
 const { convert } = require('html-to-text');
 const groupPrepositions = require('./groupPrepositions');
 const groupArticles = require('./groupArticles');
+const removeMeaningless = require('./removeMeaningless');
 
 const url = 'https://en.wikipedia.org/wiki/European_Union';
 
@@ -11,6 +12,6 @@ const url = 'https://en.wikipedia.org/wiki/European_Union';
   const phrases = text.split('.');
   const words = phrases.map(phrase => phrase.split(/\s/).filter(word => word));
   const realWordsPhrases = words.filter(phrase => /[A-Z]/.test(phrase[0] && phrase[0].charAt(0)));
-  const groups = realWordsPhrases.map(phrase => groupPrepositions(groupArticles(phrase)));
+  const groups = realWordsPhrases.map(phrase => groupPrepositions(groupArticles(removeMeaningless(phrase))));
   console.log(JSON.stringify(groups));
 })();
---
+++ src/removeMeaningless.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const meaninglessList = fs
+  .readFileSync(`${__dirname}/resources/meaningless.txt`)
+  .toString()
+  .split('\n')
+  .filter(x => x);
+
+const removeMeaningless = phrase => phrase
+  .filter(
+    word => !meaninglessList.includes(word.replace(/[^A-Za-z]/g, '').toLowerCase()),
+  );
+
+module.exports = removeMeaningless;
