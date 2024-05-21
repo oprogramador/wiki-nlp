9d3c02b6bd90539be34ae6ff1a68e3c042f7256e
oprogramador <uomodislesia@gmail.com>
2024-05-21 05:58:12 +0200

    use wikijs

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,11 @@
-const request = require('superagent');
-const { convert } = require('html-to-text');
+const wiki = require('wikijs').default;
 const groupPrepositions = require('./groupPrepositions');
 const groupArticles = require('./groupArticles');
 const removeMeaningless = require('./removeMeaningless');
 
-const url = 'https://en.wikipedia.org/wiki/European_Union';
-
 (async () => {
-  const data = await request(url);
-  const text = convert(data.text);
+  const data = await wiki().page('European Union');
+  const text = await data.rawContent();
   const phrases = text.split('.');
   const words = phrases.map(phrase => phrase.split(/\s/).filter(word => word));
   const realWordsPhrases = words.filter(phrase => /[A-Z]/.test(phrase[0] && phrase[0].charAt(0)));
