ba27c748638cc1130503800013b732b466c5e833
oprogramador <uomodislesia@gmail.com>
2024-05-30 06:23:39 +0200

    handle punctuation before a word

--- src/convertPunctuation.js
+++ src/convertPunctuation.js
@@ -1,15 +1,30 @@
+const _ = require('lodash');
+
 const list = [
   ',',
   ':',
   '%',
+  '€',
 ];
 
 const convertPunctuation = phrase => phrase
-  .map(
-    word => (list.includes(word.slice(-1))
-      ? [word.slice(0, -1), word.slice(-1)]
-      : [word]),
-  )
+  .map((word) => {
+    let start = '';
+    let middle = word;
+    let end = '';
+    const first = _.first(word);
+    const last = _.last(word);
+    if (list.includes(last)) {
+      middle = middle.slice(0, -1);
+      end = last;
+    }
+    if (list.includes(first)) {
+      start = first;
+      middle = middle.slice(1);
+    }
+
+    return [start, middle, end].filter(x => x);
+  })
   .flat();
 
 module.exports = convertPunctuation;
--- src/tests/convertPunctuation.spec.js
+++ src/tests/convertPunctuation.spec.js
@@ -24,4 +24,34 @@ describe('convertPunctuation', () => {
       'Bob',
     ]);
   });
+
+  it('groups with a euro sign', () => {
+    const words = ['Alan', 'has', '€100'];
+
+    const result = convertPunctuation(words);
+
+    expect(result).to.deep.equal([
+      'Alan',
+      'has',
+      '€',
+      '100',
+    ]);
+  });
+
+  it('groups with both a comma and euro sign', () => {
+    const words = ['Alan', 'has', '€100,', 'and', 'a', 'car'];
+
+    const result = convertPunctuation(words);
+
+    expect(result).to.deep.equal([
+      'Alan',
+      'has',
+      '€',
+      '100',
+      ',',
+      'and',
+      'a',
+      'car',
+    ]);
+  });
 });
