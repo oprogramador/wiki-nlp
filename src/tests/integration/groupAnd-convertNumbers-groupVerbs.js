af2f68d0aef6f27bdd2ec6b935a54de12f9a78d8
oprogramador <uomodislesia@gmail.com>
2024-06-02 23:48:16 +0200

    test integration: groupAnd & convertNumbers & groupVerbs

---
+++ src/tests/integration/groupAnd-convertNumbers-groupVerbs.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+const groupAnd = require('../../groupAnd');
+const convertNumbers = require('../../convertNumbers');
+const groupVerbs = require('../../groupVerbs');
+const expect = require('../expect');
+
+describe('groupAnd & convertNumbers & groupVerbs', () => {
+  it('converts', () => {
+    const words = ['Alan', 'has', 'ten', 'apples', ',', 'banabas', ',', 'and', 'oranges'];
+
+    const result = _.flow(
+      groupAnd,
+      convertNumbers,
+      groupVerbs,
+    )(words);
+
+    expect(result).to.deep.equal([
+      {
+        groupType: 'verb',
+        object: [
+          {
+            groupType: 'quantity',
+            item: {
+              groupType: 'and',
+              members: [
+                'apples',
+                'banabas',
+                'oranges',
+              ],
+            },
+            value: 10,
+          },
+        ],
+        subject: [
+          'Alan',
+        ],
+        verb: 'has',
+      },
+    ]);
+  });
+});
