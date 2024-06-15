0eeb00694dc80a958330e66708ee0d6fdd8d4ace
oprogramador <uomodislesia@gmail.com>
2024-06-15 22:21:49 +0200

    skip the period

--- src/convertDateRanges.js
+++ src/convertDateRanges.js
@@ -1,10 +1,15 @@
+const _ = require('lodash');
+
 const convertDateRanges = phrase => phrase.reduce(
   (accumulator, current) => {
     if (/–/.test(current)) {
       const split = current.split('–');
+      const beforeLast = accumulator.slice(-2, -1)[0] || {};
+      const last = _.last(accumulator);
+      const isPeriodMentioned = beforeLast === 'the' && last === 'period';
 
       return [
-        ...accumulator,
+        ...(isPeriodMentioned ? accumulator.slice(0, -2) : accumulator),
         {
           groupType: 'date',
           maxYear: split[1],
