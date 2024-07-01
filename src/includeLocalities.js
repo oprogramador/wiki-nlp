c79eb8346665988b9ef9593198ebaf4e174c88e4
oprogramador <uomodislesia@gmail.com>
2024-07-01 04:45:29 +0200

    includeLocalities - simplify

--- src/includeLocalities.js
+++ src/includeLocalities.js
@@ -5,14 +5,15 @@ const includeLocalities = (phrase) => {
   if (!object) {
     return phrase;
   }
-  const foundInObject = phrase[0].object.find(word => word.groupType === 'locality' && word.preposition === 'in');
-  const foundInSubject = phrase[0].subject.find(word => word.groupType === 'locality' && word.preposition === 'in');
+  const match = word => word.groupType === 'locality' && word.preposition === 'in';
+  const foundInObject = phrase[0].object.find(match);
+  const foundInSubject = phrase[0].subject.find(match);
   const where = _.omit(foundInObject || foundInSubject, ['preposition']);
 
   return [{
     ...phrase[0],
-    object: object.filter(word => JSON.stringify(word) !== JSON.stringify(foundInObject)),
-    subject: subject.filter(word => JSON.stringify(word) !== JSON.stringify(foundInSubject)),
+    object: object.filter(word => !_.isEqual(word, foundInObject)),
+    subject: subject.filter(word => !_.isEqual(word, foundInSubject)),
     ...(foundInObject || foundInSubject ? { where } : {}),
   }];
 };
