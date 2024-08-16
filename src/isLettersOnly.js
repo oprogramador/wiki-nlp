2e307d285c1fe49d966d19e2c6da91a86e38ef0f
oprogramador <uomodislesia@gmail.com>
2024-08-16 06:39:30 +0200

    remove a duplicated letter

--- src/isLettersOnly.js
+++ src/isLettersOnly.js
@@ -7,7 +7,7 @@ const isLettersOnly = (item) => {
     return false;
   }
 
-  return /^[a-zA-Záéíóúâêôãõàèìòòäöü-]+$/.test(item);
+  return /^[a-zA-Záéíóúâêôãõàèìòäöü-]+$/.test(item);
 };
 
 module.exports = isLettersOnly;
