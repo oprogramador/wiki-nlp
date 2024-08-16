13a2356b122155886920cd2c47e49b24b37728ae
oprogramador <uomodislesia@gmail.com>
2024-08-16 03:24:55 +0200

    higher numbers

--- src/numberResources.js
+++ src/numberResources.js
@@ -13,6 +13,21 @@ const wordsToNumbers = {
   ten: 10,
   eleven: 11,
   twelve: 12,
+  thirteen: 13,
+  fourteen: 14,
+  fifteen: 15,
+  sixteen: 16,
+  seventeen: 17,
+  eighteen: 18,
+  nineteen: 19,
+  twenty: 20,
+  thirty: 30,
+  forty: 40,
+  fifty: 50,
+  sixty: 60,
+  seventy: 70,
+  eighty: 80,
+  ninety: 90,
 };
 
 const largeNumbers = {
