c02100e576d623270199a903168b461f432e3893
oprogramador <uomodislesia@gmail.com>
2024-09-21 12:24:48 +0200

    getLast in convertBetween

--- src/convertBetween.js
+++ src/convertBetween.js
@@ -1,18 +1,18 @@
-const _ = require('lodash');
 const toLowerCase = require('./toLowerCase');
 const {
-  getBeforeLast,
-  getBeforeBeforeLast,
-  getBeforeBeforeBeforeLast,
+  getLast,
   withoutFirst,
 } = require('./listUtils');
 
 const convertBetween = phrase => phrase
   .reduce((accumulator, current) => {
-    const beforeBeforeBeforeLast = getBeforeBeforeBeforeLast(accumulator);
-    const beforeBeforeLast = getBeforeBeforeLast(accumulator);
-    const beforeLast = getBeforeLast(accumulator);
-    const last = _.last(accumulator);
+    const [
+      beforeBeforeBeforeLast,
+      beforeBeforeLast,
+      beforeLast,
+      last,
+    ] = getLast(accumulator, 4);
+
     if (
       toLowerCase(beforeBeforeBeforeLast) === 'between'
       && beforeLast === ','
